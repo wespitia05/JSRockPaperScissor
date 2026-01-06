0388a0f40eb61b0da03b9c75aa96e757d6c0f52a
william <espiw@farmingdale.edu>
2026-01-06 18:13:49 -0500

    rps functionality complete, will add other features in

--- rps.js
+++ rps.js
@@ -1,1 +1,35 @@
 // rock, paper, scissor game functionality //
+
+const choices = ["rock", "paper", "scissors"];
+const playerDisplay = document.getElementById("playerDisplay");
+const computerDisplay = document.getElementById("computerDisplay");
+const resultDisplay = document.getElementById("resultDisplay");
+
+function playGame(playerChoice) {
+    // this will have the computer randomly choose between 0-2
+    // 0 = rock, 1 = paper, 2 = scissors
+    const computerChoice = choices[Math.floor(Math.random() * 3)];
+
+    let result = "";
+
+    if(playerChoice === computerChoice) {
+        result = "IT'S A TIE!";
+    }
+    else {
+        switch(playerChoice) {
+            case "rock":
+                result = (computerChoice === "scissors") ? "YOU WIN!" : "YOU LOSE!";
+                break;
+            case "paper":
+                result = (computerChoice === "rock") ? "YOU WIN!" : "YOU LOSE!";
+                break;
+            case "scissors":
+                result = (computerChoice === "paper") ? "YOU WIN!" : "YOU LOSE!";
+                break;
+        }
+    }
+
+    playerDisplay.textContent = `PLAYER: ${playerChoice}`;
+    computerDisplay.textContent = `COMPUTER: ${computerChoice}`;
+    resultDisplay.textContent = result;
+}
